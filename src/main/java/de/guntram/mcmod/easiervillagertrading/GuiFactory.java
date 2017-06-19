6b8ab6b2da71ac65b2dde22065628dbd8ce629c8
Guntram Blohm <gbl@bso2001.com>
2017-06-19 20:56:50 +0200

    make the mod 1.12 compatible

--- src/main/java/de/guntram/mcmod/easiervillagertrading/GuiFactory.java
+++ src/main/java/de/guntram/mcmod/easiervillagertrading/GuiFactory.java
@@ -6,6 +6,12 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.fml.client.IModGuiFactory;
 
 public class GuiFactory implements IModGuiFactory {
+
+    @Override
+    public boolean hasConfigGui() {
+        return true;
+    }
+    
     @Override
     public void initialize(final Minecraft minecraftInstance) {
     }
@@ -13,6 +19,11 @@ public class GuiFactory implements IModGuiFactory {
     @Override
     public Class<? extends GuiScreen> mainConfigGuiClass() {
         return de.guntram.mcmod.easiervillagertrading.GuiConfig.class;
+    }
+    
+    @Override
+    public GuiScreen createConfigGui(GuiScreen parentScreen) {
+        return new GuiConfig(parentScreen);
     }
     
     @Override
